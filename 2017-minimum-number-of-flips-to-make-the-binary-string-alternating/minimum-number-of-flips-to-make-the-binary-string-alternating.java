53d3b1ba24d3a30e77ebd0b133c555df22dafc9f
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-03-07 19:40:29 +0530

    Time: 12 ms (100.00%) | Memory: 47.3 MB (60.59%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -1,33 +1,60 @@
 class Solution {
     public int minFlips(String s) {
         int n = s.length();
-        String str = s + s;
-        
-        StringBuilder alt1 = new StringBuilder();
-        StringBuilder alt2 = new StringBuilder();
-        
-        for(int i = 0; i < str.length(); i++){
-            alt1.append(i % 2 == 0 ? '0' : '1');
-            alt2.append(i % 2 == 0 ? '1' : '0');
-        }
-        
-        int l = 0, diff1 = 0, diff2 = 0, ans = Integer.MAX_VALUE;
-        
-        for(int r = 0; r < str.length(); r++){
-            if(str.charAt(r) != alt1.charAt(r)) diff1++;
-            if(str.charAt(r) != alt2.charAt(r)) diff2++;
-            
-            if(r - l + 1 > n){
-                if(str.charAt(l) != alt1.charAt(l)) diff1--;
-                if(str.charAt(l) != alt2.charAt(l)) diff2--;
-                l++;
-            }
-            
-            if(r - l + 1 == n){
-                ans = Math.min(ans, Math.min(diff1, diff2));
+
+        int cost1 = 0;
+        int cost2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                if (s.charAt(i) == '1')
+                    cost1++;
+                else
+                    cost2++;
+            } else {
+                if (s.charAt(i) == '0')
+                    cost1++;
+                else
+                    cost2++;
             }
         }
-        
-        return ans;
+
+        int result = Math.min(cost1, cost2);
+
+        if (n % 2 == 1) {
+            String doubled = s + s;
+
+            for (int i = 1; i < n; i++) {
+                char leaving = doubled.charAt(i - 1);
+                char entering = doubled.charAt(i + n - 1);
+
+                if ((i - 1) % 2 == 0) {
+                    if (leaving == '1')
+                        cost1--;
+                    else
+                        cost2--;
+                } else {
+                    if (leaving == '0')
+                        cost1--;
+                    else
+                        cost2--;
+                }
+
+                if ((i + n - 1) % 2 == 0) {
+                    if (entering == '1')
+                        cost1++;
+                    else
+                        cost2++;
+                } else {
+                    if (entering == '0')
+                        cost1++;
+                    else
+                        cost2++;
+                }
+
+                result = Math.min(result, Math.min(cost1, cost2));
+            }
+        }
+        return result;
     }
 }
