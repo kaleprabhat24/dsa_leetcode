60a8c031b88b260e4f285b18c39b7d180d849f96
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-03-12 16:15:44 +0530

    Time: 9 ms (35.66%) | Memory: 46.5 MB (82.00%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+        String rev = new StringBuilder(s).reverse().toString();
+        String str = s + "#" + rev;
+
+        int[] lps = new int[str.length()];
+        for (int i = 1; i < str.length(); i++) {
+            int j = lps[i - 1];
+            while (j > 0 && str.charAt(i) != str.charAt(j)) {
+                j = lps[j - 1];
+            }
+            if (str.charAt(i) == str.charAt(j)) j++;
+            lps[i] = j;
+        }
+
+        int len = lps[str.length() - 1];
+        return rev.substring(0, s.length() - len) + s;
+    }
+}
