40ea6a8a17d8e90311d63f0fb4fbbf51d2600483
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-03-15 00:46:35 +0530

    Time: 0 ms (100.00%) | Memory: 61.4 MB (52.57%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,35 +1,20 @@
 class Solution {
-
-    // Function to reverse part of the array between given indices
-    void reverseArray(int[] nums, int start, int end) {
-        // Swap elements until start meets end
-        while (start < end) {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n; 
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+    private void reverse( int[] nums, int start, int end){
+        while( start< end){
             int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
+            nums[start]= nums[end];
+            nums[end]=temp;
             start++;
             end--;
         }
     }
-
-    public void rotate(int[] nums, int k) {
-
-        // Get array length
-        int n = nums.length;
-
-        // Edge case: do nothing if k is 0
-        if (k == 0) return;
-
-        // Normalize k if greater than n
-        k = k % n;
-
-        // Step 1: reverse entire array
-        reverseArray(nums, 0, n - 1);
-
-        // Step 2: reverse first k elements
-        reverseArray(nums, 0, k - 1);
-
-        // Step 3: reverse remaining n-k elements
-        reverseArray(nums, k, n - 1);
-    }
 }
+
+    
