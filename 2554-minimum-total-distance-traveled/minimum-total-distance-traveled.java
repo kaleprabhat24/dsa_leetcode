8058c9483598de7992c57c2b14d54f77497686f3
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-04-14 20:49:14 +0530

    Time: 31 ms (88.71%) | Memory: 44.7 MB (66.13%) - LeetSync

---
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
+        Collections.sort(robot);
+        Arrays.sort(factory, (a, b) -> a[0] - b[0]);
+
+        int n = robot.size();
+        int m = factory.length;
+
+        long[][] dp = new long[n + 1][m + 1];
+
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], Long.MAX_VALUE / 2);
+        }
+
+        dp[0][0] = 0;
+
+        for (int j = 1; j <= m; j++) {
+            int pos = factory[j - 1][0];
+            int limit = factory[j - 1][1];
+
+            for (int i = 0; i <= n; i++) {
+                // Case 1: skip factory
+                dp[i][j] = dp[i][j - 1];
+
+                long cost = 0;
+
+                // Try assigning k robots
+                for (int k = 1; k <= limit && i - k >= 0; k++) {
+                    cost += Math.abs(robot.get(i - k) - pos);
+                    dp[i][j] = Math.min(dp[i][j],
+                            dp[i - k][j - 1] + cost);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
