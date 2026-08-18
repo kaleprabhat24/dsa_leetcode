5f41ea3d72df76e735cc467fcfc60a877598c69a
Prabhat Kale <152779947+kaleprabhat24@users.noreply.github.com>
2026-08-18 19:18:46 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (49.77%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -4,12 +4,11 @@ class Solution {
         long sum = 0;
 
         for(int i = 0; i<nums.length; i++){
-            sum += nums[i];
+            sum+= nums[i];
         }
-
         long exp = n*(n+1)/2;
 
-        return(int)(exp-sum);
-        
+        return (int)(exp-sum);
+
     }
 }
